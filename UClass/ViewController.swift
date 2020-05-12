a8a10a7ef0c3472a4257440c0f403a442c181f8a
Amos Ebenhaezer <amosehaezer@gmail.com>
2020-05-12 15:55:46 +0700

    Initial Commit

---
+++ UClass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UClass
+//
+//  Created by Amos Ebenhaezer on 12/05/20.
+//  Copyright © 2020 Amos Ebenhaezer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
