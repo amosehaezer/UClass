7ad73bd99c47fe9e5a3c0d371dd7f0022096548f
Amos Ebenhaezer <amosehaezer@gmail.com>
2020-05-26 16:04:52 +0700

    add notification page

---
+++ UClass/View/NotificationCell.swift
@@ -0,0 +1,22 @@
+//
+//  NotificationCell.swift
+//  UClass
+//
+//  Created by Josia Mannuel on 26/05/20.
+//  Copyright © 2020 Amos Ebenhaezer. All rights reserved.
+//
+
+import UIKit
+
+class NotificationCell: UITableViewCell {
+
+    @IBOutlet weak var profilePic: UIView!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var time: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
